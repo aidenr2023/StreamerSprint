faa4301ec508516b25d89e85fb00cc292dfe6051
LeoSanta183 <santazleo2004@gmail.com>
2024-09-23 14:44:03 -0400

    Added Procedural Generation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
 
     public bool isHoldingJump = false;
     public float maxHoldJumpTime = 0.4f;
+    public float maxFRHoldTime = 0.4f;
     public float holdJumpTimer = 0.0f;
 
     public float jumpGroundThreshold = 1;
@@ -96,6 +97,7 @@ public class PlayerMovement : MonoBehaviour
         {
             float velocityRatio = velocity.x / maxXVelocity;
             acceleration = maxAcceleration * (1 - velocityRatio);
+            maxHoldJumpTime = maxFRHoldTime * velocityRatio;
 
             velocity.x += acceleration * Time.fixedDeltaTime;
             if (velocity.x >= maxXVelocity)
