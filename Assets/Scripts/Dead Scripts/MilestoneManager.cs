cdc3d1a16d6c94633454cacee73088c9148ed182
Aiden Richmond <aiden.richmond56@gmail.com>
2024-11-04 19:08:26 -0500

    For my  Pookie Bears Leo and Brian

--- Assets/Scripts/Dead Scripts/MilestoneManager.cs
+++ Assets/Scripts/Dead Scripts/MilestoneManager.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MilestoneManager : MonoBehaviour
 {
     [SerializeField] private int[] subscriberMilestones;
     private int currentMilestoneIndex = 0;
 
+    // UnityEvent to notify when each milestone is reached
+    public UnityEvent<int> OnMilestoneReached;
+
     void UpdateSubscriber(int subscriberCount)
     {
-        // Check if the player has reached the next milestone
         if (currentMilestoneIndex < subscriberMilestones.Length &&
             subscriberCount >= subscriberMilestones[currentMilestoneIndex])
         {
             UnlockMilestone(currentMilestoneIndex);
-            currentMilestoneIndex++; // Move to the next milestone
+            OnMilestoneReached?.Invoke(currentMilestoneIndex);  
+            currentMilestoneIndex++;
         }
     }
 
