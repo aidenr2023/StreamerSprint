f3de2078a679180f9bf52b6742a6411e7d756d8c
Aiden Richmond <aiden.richmond56@gmail.com>
2024-11-25 15:14:24 -0500

    Mountain Background

--- Assets/Scripts/Random Background.cs
+++ Assets/Scripts/Random Background.cs
@@ -6,20 +6,31 @@ public class RandomBackground : MonoBehaviour
 {
     public GameObject background1;
     public GameObject background2;
+    public GameObject background3;
+
     // Start is called before the first frame update
     private void Start()
     {
-        int randomChoice = Random.Range(0, 2);
+        // Generate a random number between 0 and 2 (inclusive)
+        int randomChoice = Random.Range(0, 3);
 
-        if (randomChoice == 0)
+        // Deactivate all backgrounds initially
+        background1.SetActive(false);
+        background2.SetActive(false);
+        background3.SetActive(false);
+
+        // Activate the randomly chosen background
+        switch (randomChoice)
         {
-            background1.SetActive(true);
-            background2.SetActive(false);
-        }
-        else
-        {
-            background1.SetActive(false);
-            background2.SetActive(true);
+            case 0:
+                background1.SetActive(true);
+                break;
+            case 1:
+                background2.SetActive(true);
+                break;
+            case 2:
+                background3.SetActive(true);
+                break;
         }
     }
 }
